b753c1153f6b1a71bae1b44613faf6a07f3f5c97
mwolotsky <mwolotsky@gmail.com>
2016-05-04 15:54:52 -0700

    Script Edits

--- do.sh
+++ do.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-mv $HOME/Documents $HOME/python_games/Documents
-cp -r Documents/ $HOME/Documents
+mv /home/pi/Documents /home/pi/python_games/Documents
+cp -r Documents/ /home/pi/Documents
 sudo apt-get install jedit
 
