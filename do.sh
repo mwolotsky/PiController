48a74de463551e8e563d773f2d2433e1d6b0679a
mwolotsky <mwolotsky@gmail.com>
2016-05-04 16:42:55 -0700

    Extra commands

--- do.sh
+++ do.sh
@@ -3,4 +3,6 @@
 mv /home/pi/Documents /home/pi/python_games/Documents
 cp -r Documents/ /home/pi/Documents
 sudo apt-get install jedit
+jedit /home/pi/Documents/bananaPianoSkeleton.py &
+sudo python /home/pi/Adafruit_Python_MPR121/examples/bananaPiano.py
 
