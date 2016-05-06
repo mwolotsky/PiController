bf3a0c9fb1de0c937ef4510e361755bc883764c5
mwolotsky <mwolotsky@gmail.com>
2016-05-05 18:52:45 -0700

    Clean Memory

--- pi_conf.sh
+++ pi_conf.sh
@@ -15,6 +15,6 @@ mv /home/pi/Documents /home/pi/python_games/Documents
 cp bananaPiano.py /home/pi/Adafruit_Python_MPR121/examples/
 cp -r Documents/ /home/pi/Documents
 yes | sudo apt-get install jedit
+sudo apt-get clean
 jedit /home/pi/Documents/bananaPianoSkeleton.py &
 sudo python /home/pi/Adafruit_Python_MPR121/examples/bananaPiano.py
-
